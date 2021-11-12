72de573a5642021048d6f834a7e3b840bac3e885
codernishchay <nishchay.vns2000@gmaiil.com>
2021-11-12 13:43:00 -0500

    added two methods for encryption and decryption .

---
+++ backend/services/symmetric.js
@@ -0,0 +1,16 @@
+// this is going to be a endpoint where
+
+// https://medium.com/@spatocode/symmetric-encryption-in-javascript-bcb5fd14c273
+const getFileAndEncryptSy = function(req, res){
+    // get the file. 
+    // apply the algorithm 
+    // return the symmetric key 
+    // or store your key here on this app
+ 
+}
+
+const getFileAndDecryptSy = function(req, res) {
+
+ 
+}
+
