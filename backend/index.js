efdbe7f40575ebf74b2742546afc6409e2b558b5
codernishchay <nishchay.vns2000@gmaiil.com>
2021-11-13 08:53:14 -0500

    .

--- backend/index.js
+++ backend/index.js
@@ -2,30 +2,13 @@ const express = require('express')
 const fileUpload = require('express-fileupload')
 const port = 3001;
 const app = express();
-// const cors = require('cors')
-
 const cors = require("cors");
-
-
+const {getTextAndEncryptSy, EncryptUploadedFile, getTextAndDecryptSy } = require('./services/symmetric');
 app.use(cors()) 
 app.use(fileUpload())
-// app.use(cors())
-app.post('/upload', (req, res) => {
-    if(req.files == null){
-        return res.status(400).json({ msg: "No file uploaded!"})
-    }
-    // console.log(req)
-    const file = req.files.file;
-
-    file.mv(`${__dirname}/uploads/${file.name}`, err => {
-        if(err){
-            console.log(err);
-            return res.status(500).send(err);
-        }
-        res.json({ fileName: file.name, filePath: `${__dirname}/uploads/${file.name}`});
-    })
-})
-
+console.log("yes all is well ") 
+app.post('/decode' , getTextAndDecryptSy);
+app.post('/encode', getTextAndEncryptSy); 
 
 
 app.listen(port, ()=> console.log(`Server running on http://localhost:${port}`));
--- backend/services/symmetric.js
+++ backend/services/symmetric.js
@@ -1,16 +1,81 @@
-// this is going to be a endpoint where
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
 
-// https://medium.com/@spatocode/symmetric-encryption-in-javascript-bcb5fd14c273
-const getFileAndEncryptSy = function(req, res){
-    // get the file. 
-    // apply the algorithm 
-    // return the symmetric key 
-    // or store your key here on this app
- 
-}
+exports.getTextAndEncryptSy  = function(req, res) {
+  console.log("yes enterd")
+  const algorithm = "aes-192-cbc";
+  const password = "Password used to generate key";
+  const key = crypto.scryptSync(password, "salt", 24);
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
 
-const getFileAndDecryptSy = function(req, res) {
+  cipher.on("readable", () => {
+    console.log(cipher.read().toString("hex"));
+  });
 
- 
-}
+  cipher.write(req.body);
+  cipher.end();
+  res.send(cipher , password)
+};
 
+exports.getTextAndDecryptSy = function(req, res) {
+    const crypto = require("crypto");
+    const algorithm = "aes-192-cbc";
+    const password = "Password used to generate key";
+    const key = crypto.scryptSync(password, "salt", 24);
+    const iv = crypto.randomBytes(16);
+    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+  
+    decipher.on("readable", () => {
+      console.log(decipher.read().toString("utf8"));
+    });
+    decipher.write(req.body, "hex");
+    decipher.end();
+    res.send(decipher, password)
+  };
+
+// exports.EncryptUploadedFile = function(req, res){
+//     if(req.files == null){
+//         return res.status(400).json({ msg: "No file uploaded!"})
+//     }
+//     const file = req.files.file;
+//     file.mv(`${__dirname}/uploads/${file.name}`, err => {
+//         if(err){
+//             console.log(err);
+//             return res.status(500).send(err);
+//         }
+//         res.json({ fileName: file.name, filePath: `${__dirname}/uploads/${file.name}`});
+//     })
+//     encrypted = EncryptFile(fileName) ; 
+//     res.send(encrypted); 
+// }
+
+
+// const EncryptFile = function(file){
+//   console.log(file); 
+//   const algorithm = "aes-192-cbc";
+//   const key = crypto.scryptSync(password, "salt", 24);
+//   const iv = crypto.randomBytes(16);
+//   const decipher = crypto.createDecipheriv(algorithm, key, iv);
+//   const fileToEncrypt = path.join("../uploads" , file);
+//   const input = fs.createReadStream(fileToEncrypt);
+//   const output = fs.createReadStream(`${fileToEncrypt}.sec`);
+
+//   input.pipe(cipher).pipe(output);
+//   fs.unlinkSync(fileToEncrypt);
+//   console.log("Encryption successfull");
+//   return fileToEncrypt.sec ; 
+// };
+
+// exports.DecryptFile = function(req, res) {
+//   const homeDir = process.env.USERPROFILE;
+//   const fileToEncrypt = path.join(homeDir, "file-name.txt");
+//   const input = fs.createReadStream(fileToEncrypt);
+//   const output = fs.createWriteStream(
+//     fileToEncrypt.slice(0, fileToEncrypt.length - 4)
+//   );
+//   input.pipe(decipher).pipe(output);
+//   fs.unlinkSync(fileToEncrypt);
+//   console.log("Decryption successfull!!!");
+// };
